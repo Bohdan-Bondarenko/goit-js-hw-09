53d01dda1fa3089a6d48529627027619e2b12f62
Bohdan-Bondarenko <121513522+Bohdan-Bondarenko@users.noreply.github.com>
2024-01-31 23:08:53 +0000

    Deploying to gh-pages from @ Bohdan-Bondarenko/goit-js-hw-09@84d179ca80d95d8c50d71a3f658d4ab5b793c811 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function n(){return`
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function s(){return`
       <form class="feedback-form" autocomplete="off">
         <label>
           Email
@@ -10,5 +10,5 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
         </label>
         <button type="submit">Submit</button>
       </form>
-    `}const r=document.getElementById("form-container");r.innerHTML=n();const o=document.querySelector(".feedback-form"),m="feedback-form-state";o.addEventListener("input",s);o.addEventListener("submit",c);let t=i();function s(a){const{email:e,message:l}=o.elements;t={email:e.value.trim(),message:l.value.trim()},localStorage.setItem(m,JSON.stringify(t))}function i(){return JSON.parse(localStorage.getItem(m))||{}}function u(){const{email:a,message:e}=o.elements;t&&(a.value=t.email||"",e.value=t.message||"")}function c(a){a.preventDefault();const{email:e,message:l}=o.elements;if(e.value.trim()===""||l.value.trim()==="")return alert("Please fill in all the fields!");console.log({email:e.value,message:l.value}),localStorage.removeItem(m),a.currentTarget.reset(),t={}}u();
+    `}const i=document.getElementById("form-container");i.innerHTML=s();const a=document.querySelector(".feedback-form"),m="feedback-form-state";a.addEventListener("input",u);a.addEventListener("submit",f);let l=r();function u(t){let e=r();const{email:o,message:n}=a.elements;e={email:o.value.trim(),message:n.value.trim()},localStorage.setItem(m,JSON.stringify(e))}function r(){return JSON.parse(localStorage.getItem(m))||{}}function c(){const{email:t,message:e}=a.elements;l&&(t.value=l.email||"",e.value=l.message||"")}function f(t){t.preventDefault();const{email:e,message:o}=a.elements;if(e.value.trim()===""||o.value.trim()==="")return alert("Please fill in all the fields!");console.log({email:e.value,message:o.value}),localStorage.removeItem(m),t.currentTarget.reset(),l={}}c();
 //# sourceMappingURL=commonHelpers2.js.map
